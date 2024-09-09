09fedd157571777d9acecb55d913a528d4a4df32
Daniel J Blueman <daniel@quora.org>
2024-09-09 20:27:14 +0800

    Enable networking by default; various usability enhancements; additional packages

--- generate.sh
+++ generate.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -x
+#!/bin/bash -ex
+
 # mkdir -p rescue/root/lib/modules
+rm -f rescue/vmlinuz-* rescue/*.cpio.zst-*
 sudo rm -rf rescue/root && mkdir -p rescue/root && echo "force-unsafe-io" >rescue/root/.dpkg.cfg && sudo ./debirf make -o -r rescue
-sudo chmod a+r rescue/vmlinuz-* rescue/debirf-*
+sudo chmod a+r rescue/vmlinuz-* rescue/*.cpio.zst
---
+++ upload.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+function xfer() {
+   rsync -aP -e "ssh -o pubkeyauthentication=no -p 64122" "$1" "pi@localhost:/srv/tftp/eviden-rescueos/$2"
+}
+
+ssh -Nf -L64122:10.0.0.3:22 dmzsdd
+
+xfer rescue/vmlinuz-*-generic vmlinuz
+xfer rescue/*.cpio.zst initrd.img
