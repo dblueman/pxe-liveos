3765b00a86a3f0e7e0db07c4219c304fc7cddd10
Daniel J Blueman <daniel@quora.org>
2024-09-24 14:18:18 +0800

    Add proxy and CUDA path to the environent; fix upload

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 function xfer() {
-   rsync -aP --inplace -e "ssh -o pubkeyauthentication=no -p 64122" "$1" "pi@localhost:/srv/tftp/eviden-rescueos/$2"
+   rsync -P --inplace -e "ssh -o pubkeyauthentication=no -p 64122" "$1" "pi@localhost:/srv/tftp/eviden-rescueos/$2"
 }
 
 ssh -Nf -L64122:10.0.0.3:22 dmzsdd
