b0036a13a0e6d4bf64cfe6d179873e65bba2f61d
Daniel J Blueman <daniel@quora.org>
2025-02-24 10:56:41 +0800

    Integrate debirf scripts locally

--- upload.sh
+++ upload.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -e
 
 function xfer() {
-   rsync -P --inplace -e "ssh -o pubkeyauthentication=no -p 64122" "$1" "pi@localhost:/srv/tftp/eviden-rescueos/$2"
+   rsync -P --inplace -e "ssh -o pubkeyauthentication=no -p 64122" "$1" "pi@localhost:/srv/tftp/rescueos/$2"
 }
 
 ssh -Nf -L64122:10.0.0.3:22 dmzsdd
 
 xfer rescue/vmlinuz-*-generic vmlinuz
 xfer rescue/*.cpio.zst initrd.img
+rm -f rescue/vmlinuz-*-generic rescue/*.cpio.zst
+sudo rm -rf rescue/root
