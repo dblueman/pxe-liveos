11fadb0b2bc15ca9d478a92c382c30f2e5e8ad29
Daniel J Blueman <daniel@quora.org>
2025-03-17 21:03:51 +0800

    Move to Ubuntu 24.04 LTS

--- upload.sh
+++ upload.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -e
 
 function xfer() {
-   rsync -P --inplace -e "ssh -o pubkeyauthentication=no -p 64122" "$1" "pi@localhost:/srv/tftp/liveos/$2"
+   rsync -P --inplace -e "ssh -o pubkeyauthentication=no -p $1" "$2" "pi@localhost:/srv/tftp/liveos/$3"
 }
 
 ssh -Nf -L64122:10.0.0.3:22 dmzsdd
+ssh -Nf -L64123:10.22.83.2:22 dmzsdd
 
-xfer liveos/vmlinuz-*-generic vmlinuz
-xfer liveos/*.cpio.zst initrd.img
+for port in 64122 64123; do
+   xfer $port liveos/vmlinuz-*-generic vmlinuz
+   xfer $port liveos/*.cpio.zst initrd.img
+done
+
 rm -f liveos/vmlinuz-*-generic liveos/*.cpio.zst
 sudo rm -rf liveos/root
